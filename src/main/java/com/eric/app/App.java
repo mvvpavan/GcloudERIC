809abaf1f6051abf6dbb740bb13333b4895ae6eb
Pavan Matta <pavan.matta@cyberoam.com>
2015-11-05 01:25:27 +0530

    init

--- src/main/java/com/eric/app/App.java
+++ src/main/java/com/eric/app/App.java
@@ -4,10 +4,36 @@ package com.eric.app;
  * Hello world!
  *
  */
+
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.get;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        port(Integer.valueOf(System.getenv("PORT")));
+    staticFileLocation("/public");
+
+    get("/hello", (req, res) -> "Hello World");
+
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "hello.ftl");
+        }, new FreeMarkerEngine());
+
     }
 }
